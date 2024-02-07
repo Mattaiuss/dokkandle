8e59abe2f8178542a9d6d43239d8808d4e8bf35f
Galvaing Valentin <valentin.galvaing@epitech.eu>
2024-02-07 16:33:35 +0100

    JULIEN AUTOCOMPLeTION

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-var data = [];
-$.getJSON(
-  "data.json",
-  function (result) {
-    $.each(result, function (index, val) {
-      data.push(val);
-    });
-  }
-);
-console.log(data);
 
+let allDatas = [];
+(() => {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  fetch(`http://127.0.0.1:5000/`, requestOptions)
+    .then(response => response.text())
+    .then(result => allDatas = JSON.parse(result))
+    .catch(error => console.log('error', error));
+})();
+
+function showOccurences(string) {
+  document.getElementById("occurences").innerHTML = "";
+  let result = allDatas?.filter(data => data?.Nom?.toLowerCase().includes(string.toLowerCase()));
+  console.log(result);
+  for (let i = 0; i < result.length; i++) {
+    let element = result[i];
+    document.getElementById("occurences").innerHTML += "<button class='choice' onclick='fetchData(\"" + element["Nom"] + "\")'>";
+    document.getElementById("occurences").innerHTML += element["Nom"];
+    document.getElementById("occurences").innerHTML += "</button><br>";
+  }
+  return result;
+}
 
 function fetchData(value) {
   var requestOptions = {
